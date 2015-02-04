9b42bcc0adcc8a30424b7deb59adf18922c31015
Sami Abdul-Wahid <sipho.1989@gmail.com>
2015-02-03 21:29:03 -0800

    Feedforward NN while trying to get the SetNumSpikes method working for the last input type of the XOR problem.

---
+++ snn.py
@@ -0,0 +1,243 @@
+import numpy as np
+import brian as br
+#import pudb
+
+def ReadImg(number=1, letter=None, bench='LI', levels=None):
+
+    if bench == 'xor':
+        if levels == None:
+            levels = 1
+        n = 3   # Dimension of single image (no pyramid)
+        img_dims = (1, 2)
+        img = np.empty(img_dims)
+
+        '''
+            00:0 11:0 10:1 01:1
+        '''
+
+        if number == 0:
+            img[0][0] = 0
+            img[0][1] = 0
+            label = 0
+
+        elif number == 1:
+            img[0][0] = 1
+            img[0][1] = 1
+            label = 0
+
+        elif number == 2:
+            img[0][0] = 1
+            img[0][1] = 0
+            label = 1
+
+        elif number == 3:
+            img[0][0] = 0
+            img[0][1] = 1
+            label = 1
+
+    elif bench == 'mnist':
+        if levels == None:
+            levels = 4
+        n = 32  # Dimension of largest image in pyramid
+        img_dims = (n, n)
+        N = 0  # Number of filters
+        dir = 'minst/'
+        data_dir = 'on_pyr/'
+
+        for h in range(levels):
+            N += ((n / 2) - 2)**2
+            n /= 2
+
+    elif bench == 'LI':
+        if levels == None:
+            levels = 1
+        n = 3   # Dimension of single image (no pyramid)
+        img_dims = (n, n)
+        img = np.empty(img_dims)
+        N = 1
+        dir = 'li-data/'
+        data_dir = 'noise/'
+        if letter == None:
+            letter = 'L'
+
+        F = open(dir + data_dir + letter + str(number) + '.txt')
+
+        for i in range(img_dims[0]):
+            line = F.readline()
+
+            for j in range(img_dims[1]):
+                img[i][j] = int(line[j])
+
+        if letter == 'L':
+            label = 0
+        else:
+            label = 1
+
+        F.close()
+
+    return img, label
+
+def GetInSpikes(img, bench='LI'):
+
+    img_dims = np.shape(img)
+    if bench == 'LI' or bench == 'xor':
+
+        spikes = []
+        h = 0
+        x_range = range(img_dims[0])
+        y_range = range(img_dims[1])
+
+        for i in x_range:
+            for j in y_range:
+                pixel = img[i][j]
+                if pixel == 0:
+                    spikes.append((h, 6*br.ms))
+                else:
+                    spikes.append((h, 0*br.ms))
+                h += 1
+        spikes.append((h, 0*br.ms))
+
+        return spikes
+
+def d_w(S_d, S_l, S_in):
+    sd = S_d - S_in
+    sl = S_l - S_in
+
+    if len(sd) == len(sl):
+        return_val = A*np.sum(np.exp(sd) - np.exp(sl))
+
+    else:
+        return_val = None
+
+    return return_val
+
+def LowPass(S):
+    pass
+
+def P_Index(S_d, S_l):
+    return_val = 0
+
+    if len(S_d) == len(S_l):
+        for i in range(len(S_d)):
+            return_val += ma.exp(abs(S_d[i] - S_l[i]))
+    else:
+        return_val = None
+
+    return return_val
+
+def ModifyWeights(S, dv):
+    n = len(S.w[:])
+    for i in range(n):
+        weet = S.w[i]
+        weet = weet*br.volt + dv*br.mV
+        S.w[i] = weet
+
+def SetNumSpikes(T, N_h, N_o, v0, u0, bench, number, input_neurons, hidden_neurons, output_neurons, Sa, Sb, M, Mv, Mu, S_in, S_hidden, S_out, train=False, letter=None):
+
+    dv = 0.2
+    i = 0
+    while True: 
+
+        #br.run(T*br.msecond,report='text')
+        #pudb.set_trace()
+        Run(T, v0, u0, bench, number, input_neurons, hidden_neurons, output_neurons, Sa, Sb, M, Mv, Mu, S_in, S_hidden, S_out, train=True, letter=None)
+        spikes_hidden = []
+        for i in range(len(hidden_neurons)):
+            spikes_hidden.append([])
+
+        spikes_out = S_out.spikes
+        N_out = len(spikes_out)
+
+        hidden_are_set = True
+        for i in range(len(hidden_neurons)):
+            if len(S_hidden[i]) < N_h:
+                ModifyWeights(Sa[i], dv)
+                hidden_are_set = False
+            elif len(S_hidden[i]) > N_h:
+                ModifyWeights(Sa[i], -dv)
+                hidden_are_set = False
+
+        if hidden_are_set == True:
+            if N_out < N_o:
+                ModifyWeights(Sb, dv)
+            elif N_out > N_o:
+                ModifyWeights(Sb, -dv)
+            elif N_out == N_o or i == 100:
+                break
+
+        i += 1
+
+def Run(T, v0, u0, bench, number, input_neurons, hidden_neurons, output_neurons, Sa, Sb, M, Mv, Mu, S_in, S_hidden, S_out, train=False, letter=None):
+
+    hidden_neurons.v = v0
+    hidden_neurons.u = u0
+    hidden_neurons.I = 0
+    output_neurons.v = v0
+    output_neurons.u = u0
+    output_neurons.I = 0
+
+    br.forget(Sa, Sb)
+    br.reinit(states=False)
+    br.recall(Sa, Sb)
+
+    img, label = ReadImg(number=number, bench=bench, letter=letter)
+    spikes = GetInSpikes(img, bench=bench)
+    input_neurons.set_spiketimes(spikes)
+    hidden_neurons.v = v0
+    output_neurons.v = v0
+    hidden_neurons.u = u0
+    output_neurons.u = u0
+    hidden_neurons.I = 0
+    output_neurons.I = 0
+    br.run(T*br.msecond,report='text')
+
+def Plot(Mv, number):
+    #if number < 2:
+    br.plot(211)
+    #pudb.set_trace()
+    br.plot(Mv.times/br.ms,Mv[0]/br.mvolt, label=str(number))
+    br.legend()
+    #elif number < 4:
+    #    br.subplot(212)
+    #    br.plot(Mv.times/br.ms,Mv[0], label='1')
+    #    br.legend()
+    #print "SAVED!"
+    #br.savefig('plot.png')
+    br.show()
+
+#def dT_dW(T, v0, u0, bench, number, input_neurons, hidden_neurons, output_neurons, Sa, Sb, M, Mv, Mu, S_in, S_out, train=False, letter=None):
+#
+#    Run(T, v0, u0, bench, number, input_neurons, hidden_neurons, output_neurons, Sa, Sb, M, Mv, Mu, S_in, S_out, train=False, letter=None)    
+
+
+
+"""
+def Train(bench, number, input_neurons, hidden_neurons, output_neurons, Sa, Sb, M, Mv, Mu, S_in, S_out, train=False, letter=None):
+    br.reinit(states=False)
+    br.run(30*br.msecond,report='text')
+
+            if len_d > 0 and n == 0:
+
+                    Sa.w[i_a] = weet
+                else:
+                    n_c = len(Sb.w[:])
+                    i_c = br.randint(n_c)
+                    weet = Sb.w[i_c]
+                    weet = weet*br.volt + dv*br.mV
+
+                    Sb.w[i_c] = weet
+
+            elif n < 5:
+                index = in_spikes[i]
+                weet = Sb.w[index]
+                if len_l < len_d:
+                    weet = weet*br.volt + dv*br.mV
+                else:
+                    weet = weet*br.volt - dv*br.mV
+                Sb.w[index] = weet
+                i += 1
+            elif n > 4:
+                dv = dv / 2
+"""
+        
+
---
+++ xor.py
@@ -0,0 +1,182 @@
+from operator import itemgetter
+import numpy as np
+import brian as br
+import time
+import pudb
+#import winpdb
+import snn 
+import train
+
+#objects = []
+N = 1
+
+vt = -15 * br.mV
+vr = -74 * br.mV
+
+a=0.02
+b=0.2
+c=-65
+d=6
+tau=5
+bench='xor'
+levels=4
+
+N_in = 2
+N_hidden = 4
+N_out = 1
+
+
+'''     0 - Calculates number of filters    '''
+
+if bench == 'mnist':
+    levels = levels
+    n = 32  # Dimension of largest image in pyramid
+    img_dims = (n, n)
+    N = 0  # Number of filters
+    dir = 'minst/'
+    data_dir = 'on_pyr/'
+
+    for h in range(levels):
+        N += ((n / 2) - 2)**2
+        n /= 2
+
+elif bench == 'LI':
+    levels = 1
+    n = 3   # Dimension of single image (no pyramid)
+    img_dims = (n, n)
+    N = 1
+    dir = 'li-data/'
+    data_dir = 'noise/'
+
+elif bench == 'xor':
+    levels = 1
+    img_dims = (1, 2)
+    N = 1
+    #dir = 'li-data/'
+    #data_dir = 'noise/'
+
+
+simtime = 1 #duration of the simulation in s
+number = 1 #number of hidden_neurons
+a = a/br.ms
+b = b/br.ms
+c = c * br.mvolt
+d = d*br.mV/br.ms
+tau = tau*br.ms
+bench = bench
+
+eqs_hidden_neurons = '''
+    dv/dt = (0.04/ms/mV)*v**2 + (5/ms) * v + 140*mV/ms - u + ge/ms + I/ms : mvolt
+    du/dt = a*((b*v) - u) : mvolt/msecond
+    dge/dt = -ge/tau : mvolt
+    I: mvolt
+'''
+
+B = b*br.ms
+
+v0 = br.mV*br.sqrt((((5 - B) / 0.08)**2-140) / 0.04) - br.mV*((5 - B) / 0.08)
+u0 = b*v0
+
+reset = '''
+    v = c
+    u += d
+'''
+
+img = np.empty(img_dims)
+
+count = 0
+g = 2
+
+
+spikes = []
+input_neurons = br.SpikeGeneratorGroup(N_in+1, spikes)
+hidden_neurons = br.NeuronGroup(N_hidden, model=eqs_hidden_neurons, threshold=vt, reset=reset)
+output_neurons = br.NeuronGroup(N_out, model=eqs_hidden_neurons, threshold=vt, reset=reset)
+
+#objects.append(hidden_neurons)
+Sa = br.Synapses(input_neurons, hidden_neurons, model='w:1', pre='ge+=w')#, max_delay=9*br.ms)
+Sb = br.Synapses(hidden_neurons, output_neurons, model='w:1', pre='ge+=w*(2)')
+
+v0, u0 = -70*br.mV, -14*br.mV/br.msecond
+
+hidden_neurons.v = v0
+hidden_neurons.u = u0
+hidden_neurons.I = 0
+
+output_neurons.v = v0
+output_neurons.u = u0
+output_neurons.I = 0
+
+
+print "v0 = ", v0
+print "u0 = ", u0
+
+Sa[:,:]=True
+Sb[:,:]=True
+
+Sa.w[:]='8.04*(1.5+0.2*rand())*br.mV'
+Sb.w[:]='9.0*(1.5+0.2*rand())*br.mV'
+
+Sa.delay='(4)*ms'
+Sb.delay='(4)*ms'
+print "n.v0, n.u0 = ", hidden_neurons.v, ", ", hidden_neurons.u
+
+M =br.StateMonitor(output_neurons,'ge',record=0)
+Mv=br.StateMonitor(output_neurons,'v',record=0)
+Mu=br.StateMonitor(output_neurons,'u',record=0)
+
+S_in = br.SpikeMonitor(input_neurons, record=True)
+S_hidden = br.SpikeMonitor(hidden_neurons, record=True)
+S_out = br.SpikeMonitor(output_neurons, record=True)
+
+#objects.append(M)
+#objects.append(Mv)
+#objects.append(Mu)
+
+'''         TRAINING        '''
+#Net = br.Network(objects)
+#OUT = open('weights.txt', 'a')
+
+number = 3
+T = 30
+N_o = 1
+N_h = 1
+
+snn.SetNumSpikes(T, N_h, N_o, v0, u0, bench, number, input_neurons, hidden_neurons, output_neurons, Sa, Sb, M, Mv, Mu, S_in, S_hidden, S_out, train=False, letter=None)
+    #print "\tDone! for number = ", number
+    #winpdb.set_trace()
+
+#for number in range(4):
+#    snn.Run(T, v0, u0, bench, number, \
+#            input_neurons, hidden_neurons, output_neurons, \
+#            Sa, Sb, M, Mv, Mu, S_in, S_hidden, S_out, train=True, letter=None)
+#
+#    snn.Plot(Mv, number)
+#
+#br.show()
+#for number in range(4):
+#    train.ReSuMe(T, N, v0, u0, bench, number, input_neurons, hidden_neurons, output_neurons, Sa, Sb, M, Mv, Mu, S_in, S_hidden, S_out)
+
+#print "spike times: ", S_out.spikes
+
+#br.plot(210)
+"""
+dw = 0.07
+spikes_store = []
+for i in range(40):
+    snn.Run(40, v0, u0, bench, number, \
+            input_neurons, hidden_neurons, output_neurons, \
+            Sa, Sb, M, Mv, Mu, S_in, S_hidden, S_out, train=True, letter=None)
+
+    Sb.w[0] += dw
+
+    spikes_store.append([])
+    for j in range(len(S_out.spiketimes)):
+        if len(S_out.spiketimes[j]) > 0:
+            spikes_store[i].append(([i, j], S_out.spiketimes[j][0]))
+
+for i in range(len(spikes_store)):
+    for j in range(len(spikes_store[i])):
+        print spikes_store[i][j], ", ",
+    print "\n"
+"""
